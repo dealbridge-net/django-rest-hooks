b1c0da770dc474961a0eaf901115e0d01f367f4f
Sari Setianingsih <sari.thok@gmail.com>
2022-11-14 16:45:27 +0700

    Make test works again

--- runtests.py
+++ runtests.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
-
 import sys
+
 import django
 from django.conf import settings
-
 
 APP_NAME = 'rest_hooks'
 if django.VERSION < (1, 8):
     comments = 'django.contrib.comments'
 else:
     comments = 'django_comments'
+
 
 settings.configure(
     DEBUG=True,
@@ -20,22 +20,36 @@ settings.configure(
     },
     USE_TZ=True,
     ROOT_URLCONF='{0}.tests'.format(APP_NAME),
-    MIDDLEWARE_CLASSES=(
+    MIDDLEWARE=(
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
     ),
     SITE_ID=1,
     HOOK_EVENTS={},
     HOOK_THREADING=False,
     INSTALLED_APPS=(
+        'django.contrib.admin',
         'django.contrib.auth',
         'django.contrib.contenttypes',
         'django.contrib.sessions',
-        'django.contrib.admin',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
         'django.contrib.sites',
         comments,
         APP_NAME,
     ),
+    TEMPLATES=[{
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    }]
 )
 
 from django.test.utils import get_runner
